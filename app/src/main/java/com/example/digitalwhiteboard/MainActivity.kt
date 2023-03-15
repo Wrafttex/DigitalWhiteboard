8b169eec700ced5a1e6d6e1ab4a80286e4ff5087
Nikolai Kure <45030008+Wrafttex@users.noreply.github.com>
2023-03-15 21:53:15 +0100

    OpenCV Working as intended with test image

--- app/src/main/java/com/example/digitalwhiteboard/MainActivity.kt
+++ app/src/main/java/com/example/digitalwhiteboard/MainActivity.kt
@@ -1,34 +1,66 @@
 package com.example.digitalwhiteboard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.Image
 import android.os.Bundle
-import android.widget.TextView
-import com.example.digitalwhiteboard.databinding.ActivityMainBinding
+import android.view.View
+import android.widget.ImageView
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Float.max
 
-class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
+    var srcBitmap: Bitmap? = null
+    var dstBitmap: Bitmap? = null
+    private lateinit var testImage: ImageView
+    lateinit var sldSigma: SeekBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        testImage = findViewById(R.id.imageView)
+        sldSigma = findViewById(R.id.sldSigma)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        srcBitmap = BitmapFactory.decodeResource(this.resources, R.drawable.testimage)
+        dstBitmap = srcBitmap!!.copy(srcBitmap!!.config, true)
+        testImage.setImageBitmap(dstBitmap)
 
-        // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        sldSigma.setOnSeekBarChangeListener(this)
+
+
     }
 
+    fun btnFlipOnClick(view: View) {
+        myFlip(srcBitmap!!,srcBitmap!!)
+        this.doBlur()
+    }
+
+    private fun doBlur() {
+        // The SeekBar range is 0-100 convert it to 0.1-10
+        val sigma = max(0.1F, sldSigma.progress / 10F)
+
+        // This is the actual call to the blur method inside native-lib.cpp
+        this.myBlur(srcBitmap!!, dstBitmap!!, sigma)
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        this.doBlur()
+    }
     /**
      * A native method that is implemented by the 'digitalwhiteboard' native library,
      * which is packaged with this application.
      */
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {}
     external fun stringFromJNI(): String
+    external fun myFlip(bitmap: Bitmap, bitmapOut: Bitmap)
+    external fun myBlur(bitmap: Bitmap, bitmapOut: Bitmap, sigma: Float)
 
     companion object {
         // Used to load the 'digitalwhiteboard' library on application startup.
         init {
-            System.loadLibrary("digitalwhiteboard")
+            System.loadLibrary("native-lib")
         }
     }
 }
